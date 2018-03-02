1ff602513ae2d213851aaf68deb7e328e20818b7
Davide <dmigliore@chronocam.com>
2018-03-02 16:04:04 +0100

    [WIP] Updating Sophus

--- thirdparty/Sophus/FindEigen3.cmake
+++ thirdparty/Sophus/FindEigen3.cmake
@@ -63,6 +63,8 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
+      /usr/local/include
+      /opt/local/include
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
@@ -78,4 +80,3 @@ else (EIGEN3_INCLUDE_DIR)
   mark_as_advanced(EIGEN3_INCLUDE_DIR)
 
 endif(EIGEN3_INCLUDE_DIR)
-
